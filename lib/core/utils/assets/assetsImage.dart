7febe317a16c0a61eab05b9130d908dd7c1b20d0
yousef sama <ys2152000@gmail.com>
2024-05-06 19:08:31 +0300

    add app assetsImage

---
+++ lib/core/utils/assets/assetsImage.dart
@@ -0,0 +1,108 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppleLogo
+  /// assets/images/appleLogo.svg
+  static const String imagesAppleLogo = "assets/images/appleLogo.svg";
+
+  /// Assets for imagesArrowDown
+  /// assets/images/Arrow-down.svg
+  static const String imagesArrowDown = "assets/images/Arrow-down.svg";
+
+  /// Assets for imagesArrowDownIcon
+  /// assets/images/ArrowDownIcon.svg
+  static const String imagesArrowDownIcon = "assets/images/ArrowDownIcon.svg";
+
+  /// Assets for imagesArrowRight
+  /// assets/images/arrow-right.svg
+  static const String imagesArrowRight = "assets/images/arrow-right.svg";
+
+  /// Assets for imagesArrowRightIcon
+  /// assets/images/ArrowRightIcon.svg
+  static const String imagesArrowRightIcon = "assets/images/ArrowRightIcon.svg";
+
+  /// Assets for imagesBackgroundImage
+  /// assets/images/BackgroundImage.png
+  static const String imagesBackgroundImage = "assets/images/BackgroundImage.png";
+
+  /// Assets for imagesBookOpenIcon
+  /// assets/images/book-open_icon.svg
+  static const String imagesBookOpenIcon = "assets/images/book-open_icon.svg";
+
+  /// Assets for imagesCategorytest
+  /// assets/images/categorytest.svg
+  static const String imagesCategorytest = "assets/images/categorytest.svg";
+
+  /// Assets for imagesChatAltIcon
+  /// assets/images/chat-alt-icon.svg
+  static const String imagesChatAltIcon = "assets/images/chat-alt-icon.svg";
+
+  /// Assets for imagesCollection
+  /// assets/images/collection.svg
+  static const String imagesCollection = "assets/images/collection.svg";
+
+  /// Assets for imagesCollectionIcon
+  /// assets/images/collectionIcon.svg
+  static const String imagesCollectionIcon = "assets/images/collectionIcon.svg";
+
+  /// Assets for imagesFacebookLogo
+  /// assets/images/facebookLogo.svg
+  static const String imagesFacebookLogo = "assets/images/facebookLogo.svg";
+
+  /// Assets for imagesGoogleLogo
+  /// assets/images/googleLogo.svg
+  static const String imagesGoogleLogo = "assets/images/googleLogo.svg";
+
+  /// Assets for imagesHeartIcon
+  /// assets/images/heart_Icon.svg
+  static const String imagesHeartIcon = "assets/images/heart_Icon.svg";
+
+  /// Assets for imagesHeartIcon2
+  /// assets/images/heartIcon2.svg
+  static const String imagesHeartIcon2 = "assets/images/heartIcon2.svg";
+
+  /// Assets for imagesHomeIcon
+  /// assets/images/HomeIcon.svg
+  static const String imagesHomeIcon = "assets/images/HomeIcon.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/Logo.svg
+  static const String imagesLogo = "assets/images/Logo.svg";
+
+  /// Assets for imagesMinusIcon
+  /// assets/images/minusIcon.svg
+  static const String imagesMinusIcon = "assets/images/minusIcon.svg";
+
+  /// Assets for imagesPlusIcon
+  /// assets/images/plus_Icon.svg
+  static const String imagesPlusIcon = "assets/images/plus_Icon.svg";
+
+  /// Assets for imagesProductTest
+  /// assets/images/productTest.svg
+  static const String imagesProductTest = "assets/images/productTest.svg";
+
+  /// Assets for imagesSearchIcon
+  /// assets/images/searchIcon.svg
+  static const String imagesSearchIcon = "assets/images/searchIcon.svg";
+
+  /// Assets for imagesShieldCheckIcon
+  /// assets/images/shield-check-icon.svg
+  static const String imagesShieldCheckIcon = "assets/images/shield-check-icon.svg";
+
+  /// Assets for imagesShoppingBag
+  /// assets/images/shopping-bag.svg
+  static const String imagesShoppingBag = "assets/images/shopping-bag.svg";
+
+  /// Assets for imagesTwitterLogo
+  /// assets/images/twitterLogo.svg
+  static const String imagesTwitterLogo = "assets/images/twitterLogo.svg";
+
+  /// Assets for imagesUserIcon
+  /// assets/images/UserIcon.svg
+  static const String imagesUserIcon = "assets/images/UserIcon.svg";
+
+  /// Assets for imagesVariant3Icon
+  /// assets/images/Variant3Icon.svg
+  static const String imagesVariant3Icon = "assets/images/Variant3Icon.svg";
+}
+
